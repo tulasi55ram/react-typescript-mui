8c4f45effd133ed1797d25414be3bc86ddfbba26
ram-daggupati <ram.daggupati@redant.com>
2022-01-19 22:57:54 +0000

    update api tests

--- src/api/api.test.ts
+++ src/api/api.test.ts
@@ -1,24 +1,44 @@
-import { getPassengerDetails } from './index'
+import { getPassengerDetails, getPassengersByPagination } from './index'
 import fetchMock from 'jest-fetch-mock'
 import { mockPassengerDetails } from './response'
 
 const userId = '12'
 
 describe("Test API's ", () => {
-  beforeEach(() => {
-    fetchMock.resetMocks()
+  describe("Test getPassenderDetails API ", () => {
+    beforeEach(() => {
+      fetchMock.resetMocks()
+    })
+    it('Should return 200 ', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify(mockPassengerDetails))
+      const fetchData = getPassengerDetails(userId)
+      await expect(fetchData).resolves.toEqual(mockPassengerDetails)
+      expect(fetch).toBeCalledTimes(1)
+      expect(fetch).toBeCalledWith(`https://api.github.com/users/${userId}`)
+    })
+    it('Should throw an error', async () => {
+      const error = new Error('API Error')
+      fetchMock.mockRejectedValueOnce(error)
+      const fetchPassDetails = getPassengerDetails(userId)
+      await expect(fetchPassDetails).rejects.toThrow(error)
+    })
   })
-  it('Should return 200 ', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(mockPassengerDetails))
-    const fetchPassDetails = getPassengerDetails(userId)
-    await expect(fetchPassDetails).resolves.toEqual(mockPassengerDetails)
-    expect(fetch).toBeCalledTimes(1)
-    expect(fetch).toBeCalledWith(`https://api.github.com/users/${userId}`)
-  })
-  it('Should throw an error', async () => {
-    const error = new Error('API Error')
-    fetchMock.mockRejectedValueOnce(error)
-    const fetchPassDetails = getPassengerDetails(userId)
-    await expect(fetchPassDetails).rejects.toThrow(error)
+  describe("Test getPassengersByPagination API", () => {
+    beforeEach(() => {
+      fetchMock.resetMocks()
+    })
+    it('Should return 200 ', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify(mockPassengerDetails))
+      const fetchDataByPagination = getPassengersByPagination()
+      await expect(fetchDataByPagination).resolves.toEqual(mockPassengerDetails)
+      expect(fetch).toBeCalledTimes(1)
+      expect(fetch).toBeCalledWith(`https://api.instantwebtools.net/v1/passenger?page=0&size=10`)
+    })
+    it('Should throw an error', async () => {
+      const error = new Error('API Error')
+      fetchMock.mockRejectedValueOnce(error)
+      const fetchPassDetails = getPassengerDetails(userId)
+      await expect(fetchPassDetails).rejects.toThrow(error)
+    })
   })
 })
