cf864abd52a2e54d03aa0753f8ec31e556eb3c03
ram-daggupati <ram.daggupati@redant.com>
2022-01-19 23:31:24 +0000

    beautifying types

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,27 +1,25 @@
 export interface IMakeRequest {
-  url: string;
-  data?: any;
-  type?: string;
+  url: string
+  data?: any
+  type?: string
 }
 
 export interface IPassengerDetails {
-  [key: string]: string | number | boolean | null;
+  [key: string]: string | number | boolean | null
 }
-
 
 export interface IPassengerAirline {
-  [key: string]: string | number;
+  [key: string]: string | number
 }
 export interface IPassengerData {
-  _id: string;
-  name: string;
-  trips: number;
-  airline: Array<IPassengerAirline>;
+  _id: string
+  name: string
+  trips: number
+  airline: IPassengerAirline[]
 }
 
-
 export interface IPassengers {
-  totalPassengers: number;
-  totalPages: number;
-  data: Array<IPassengerData>;
+  totalPassengers: number
+  totalPages: number
+  data: IPassengerData[]
 }
